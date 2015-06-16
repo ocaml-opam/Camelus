130a471d49a29a8c2a1dbdf24c456241ab098f89
Louis Gesbert <louis.gesbert@ocamlpro.com>
2015-06-16 18:15:24 +0900

    Add README and copyright

---
+++ README.md
@@ -0,0 +1,29 @@
+## opam-ci: reports on repository pull-requests to guide merge decisions
+
+This bot runs as an HTTP service that wakes up through a GitHub webhook. It's
+based on cohttp (and lwt) for the server part, ocaml-git for checking out the
+states corresponding to the pull-request, opam-lib (and dose) for linting and
+checking installable packages, and ocaml-github to post back the reports.
+
+Dependencies: `cohttp`, `opam-lib`, `git`, `yojson`, `github`.
+
+The program expects a `opam-ci.conf` in the working directory, with the
+following format:
+
+```
+name: "opam-ci"
+port: 8122
+token: "xxx"
+secret: "yyy"
+```
+
+The name and token correspond to the GitHub account and token to use (only
+public repository access is needed). The port and secret should correspond to
+the webhook configuration, and the service is at `/opam-ci`, so your
+configuration should look like:
+- http://opam.ocaml.org:8122/opam-ci
+- `secret` must be identical, it's used to authenticate requests coming from
+  your repository on GitHub
+
+The repository is cloned (bare) under `name%repo.git/.git` in the current
+directory, _e.g._ `opam%opam-repository.git/.git`.
--- opam_ci.ml
+++ opam_ci.ml
@@ -1,3 +1,18 @@
+(**************************************************************************)
+(*                                                                        *)
+(*    Copyright 2015 OCamlPro                                             *)
+(*                                                                        *)
+(*  All rights reserved.This file is distributed under the terms of the   *)
+(*  GNU Lesser General Public License version 3.0 with linking            *)
+(*  exception.                                                            *)
+(*                                                                        *)
+(*  OPAM is distributed in the hope that it will be useful, but WITHOUT   *)
+(*  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY    *)
+(*  or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public        *)
+(*  License for more details.                                             *)
+(*                                                                        *)
+(**************************************************************************)
+
 open Lwt.Infix
 
 let log fmt = OpamConsole.msg (fmt ^^ "\n%!")
