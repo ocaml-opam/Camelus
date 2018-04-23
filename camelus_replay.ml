7c8b456007084685d7027a8b087250547a12dd45
Louis Gesbert <louis.gesbert@ocamlpro.com>
2018-04-23 15:38:38 +0200

    code cleanup

--- camelus_replay.ml
+++ camelus_replay.ml
@@ -54,37 +54,6 @@ let replay_upgrade num =
                 branch dest_branch
                 ?message title
 
-let replay_upgrade num =
-  let%lwt gitstore = match%lwt RepoGit.get repo with
-    | Ok r -> Lwt.return r
-    | Error e -> Lwt.fail (Failure "Repository loading failed")
-  in
-  let%lwt p = get_pr num in
-  let merge_sha = match p.pull_merged_at, p.pull_merge_commit_sha with
-    | Some _, Some h -> h
-    | _ -> failwith "No merge SHA found"
-  in
-  let merge_parent_sha = merge_sha^"^" in
-    log "Upgrading branch from %s to %s"
-      merge_parent_sha merge_sha;
-    let%lwt new_branch =
-      FormatUpgrade.run base_branch dest_branch
-        merge_parent_sha merge_sha gitstore
-        repo
-    in
-      match new_branch with
-        | None -> Lwt.return_unit
-        | Some (branch, msg) ->
-            let title, message =
-              match OpamStd.String.cut_at msg '\n' with
-                | Some (t, m) -> t, Some (String.trim m)
-                | None -> "Merge changes from 1.2 format repo", None
-            in
-              Github_comment.pull_request
-                ~name ~token repo
-                branch dest_branch
-                ?message title
-
 let replay_check num =
   let%lwt gitstore = match%lwt RepoGit.get repo with
     | Ok r -> Lwt.return r
