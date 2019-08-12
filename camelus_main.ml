706c42f094789ee03f2db51030f7fac4a92f3818
Louis Gesbert <louis.gesbert@ocamlpro.com>
2019-08-12 11:14:21 +0200

    Fix parallelism entry-point

--- camelus_main.ml
+++ camelus_main.ml
@@ -21,6 +21,60 @@ open Camelus_lib
 
 let log fmt = OpamConsole.msg (fmt ^^ "\n%!")
 
+let handler conf gitstore = function
+  | `Pr pr when List.mem `Pr_checker conf.Conf.roles ->
+    (log "=> PR #%d received \
+          (onto %s/%s#%s from %s/%s#%s, commit %s over %s)"
+       pr.number
+       pr.base.repo.user pr.base.repo.name pr.base.ref
+       pr.head.repo.user pr.head.repo.name pr.head.ref
+       pr.head.sha pr.base.sha;
+     try%lwt
+       let%lwt report = PrChecks.run pr gitstore in
+       Github_comment.push_report
+         ~name:conf.Conf.name
+         ~token:conf.Conf.token
+         ~report
+         pr
+     with exn ->
+       log "Check failed: %s" (Printexc.to_string exn);
+       let%lwt _ =
+         Github_comment.push_status
+           ~name:conf.Conf.name ~token:conf.Conf.token pr
+           ~text:"Could not complete" `Failure
+       in
+       Lwt.return_unit)
+  | `Push p when List.mem `Push_upgrader conf.Conf.roles ->
+    (log "=> Push received (head %s onto %s)"
+       p.push_head p.push_ancestor;
+     let auth = conf.Conf.name, Github.Token.to_string conf.Conf.token in
+     let%lwt pr_branch =
+       try%lwt
+         FormatUpgrade.run conf.Conf.base_branch conf.Conf.dest_branch
+           p.push_ancestor p.push_head gitstore
+           { p.push_repo with auth = Some auth }
+       with exn ->
+         log "Upgrade commit failed: %s" (Printexc.to_string exn);
+         Lwt.return None
+     in
+     match pr_branch with
+     | None -> Lwt.return_unit
+     | Some (branch, msg) ->
+       let title, message =
+         match OpamStd.String.cut_at msg '\n' with
+         | Some (t, m) -> t, Some (String.trim m)
+         | None -> "Merge changes from 1.2 format repo", None
+       in
+       try%lwt
+         Github_comment.pull_request
+           ~name:conf.Conf.name ~token:conf.Conf.token conf.Conf.repo
+           branch conf.Conf.dest_branch
+           ?message title
+       with exn ->
+         log "Pull request failed: %s" (Printexc.to_string exn);
+         Lwt.return_unit)
+  | _ -> Lwt.return_unit
+
 let () =
   Logs.(set_reporter (format_reporter ()); set_level (Some Info));
   let conf =
@@ -31,75 +85,17 @@ let () =
         (OpamFile.to_string f) (Printexc.to_string e);
       exit 3
   in
-  let auth = conf.Conf.name, Github.Token.to_string conf.Conf.token in
   let event_stream, event_push = Lwt_stream.create () in
-  let pr_checker = List.mem `Pr_checker conf.Conf.roles in
-  let push_upgrader = List.mem `Push_upgrader conf.Conf.roles in
   let rec check_loop gitstore =
-    (* The checks are done concurrently *)
-    let () =
-      Lwt.async @@
-      fun () ->
-      try%lwt
-        match%lwt Lwt_stream.next event_stream with
-        | `Pr pr when pr_checker ->
-          (log "=> PR #%d received \
-                (onto %s/%s#%s from %s/%s#%s, commit %s over %s)"
-             pr.number
-             pr.base.repo.user pr.base.repo.name pr.base.ref
-             pr.head.repo.user pr.head.repo.name pr.head.ref
-             pr.head.sha pr.base.sha;
-           try%lwt
-             let%lwt report = PrChecks.run pr gitstore in
-             Github_comment.push_report
-               ~name:conf.Conf.name
-               ~token:conf.Conf.token
-               ~report
-               pr
-           with exn ->
-             log "Check failed: %s" (Printexc.to_string exn);
-             let%lwt _ =
-               Github_comment.push_status
-                 ~name:conf.Conf.name ~token:conf.Conf.token pr
-                 ~text:"Could not complete" `Failure
-             in
-             Lwt.return_unit)
-        | `Push p when push_upgrader ->
-          (log "=> Push received (head %s onto %s)"
-             p.push_head p.push_ancestor;
-           let%lwt pr_branch =
-             try%lwt
-               FormatUpgrade.run conf.Conf.base_branch conf.Conf.dest_branch
-                 p.push_ancestor p.push_head gitstore
-                 { p.push_repo with auth = Some auth }
-             with exn ->
-               log "Upgrade commit failed: %s" (Printexc.to_string exn);
-               Lwt.return None
-           in
-           match pr_branch with
-           | None -> Lwt.return_unit
-           | Some (branch, msg) ->
-             let title, message =
-               match OpamStd.String.cut_at msg '\n' with
-               | Some (t, m) -> t, Some (String.trim m)
-               | None -> "Merge changes from 1.2 format repo", None
-             in
-             try%lwt
-               Github_comment.pull_request
-                 ~name:conf.Conf.name ~token:conf.Conf.token conf.Conf.repo
-                 branch conf.Conf.dest_branch
-                 ?message title
-             with exn ->
-               log "Pull request failed: %s" (Printexc.to_string exn);
-               Lwt.return_unit)
-        | _ -> Lwt.return_unit
-      with
-      | Lwt_stream.Empty -> exit 0
-      | exn ->
-        log "Event handler failed: %s" (Printexc.to_string exn);
-        Lwt.return_unit
-    in
-    check_loop gitstore
+    match%lwt Lwt_stream.next event_stream with
+    | exception Lwt_stream.Empty -> exit 0
+    | exception exn ->
+      log "Event handler failed: %s" (Printexc.to_string exn);
+      Lwt.return_unit
+    | event ->
+      (* The checks are done concurrently *)
+      Lwt.async (fun () -> handler conf gitstore event);
+      check_loop gitstore
   in
   let handler event =
     let%lwt () =
